1e1ee2280748a05ee2a644373a8b1d1179fd45da
Luis Almeida <lferreira646@gmail.com>
2022-09-29 14:45:31 -0300

    Desafio ORM

---
+++ src/main/java/com/desafio/modelodominioORM/entities/Atividade.java
@@ -0,0 +1,93 @@
+package com.desafio.modelodominioORM.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	private Double preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante",
+			joinColumns = @JoinColumn(name = "atividade_id"), 
+			inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
+	
+	@OneToMany(mappedBy = "atividade")
+	private Set<Bloco> blocos = new HashSet<>();
+	
+	public Atividade() {
+	}
+
+	public Atividade(Integer id, String name, String descricao, Double preco) {
+		this.id = id;
+		this.nome = name;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return nome;
+	}
+
+	public void setName(String name) {
+		this.nome = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Set<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	
+	
+	
+}
